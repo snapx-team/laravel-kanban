23783574719323b04b1fbbe8764ee175f84b1ee0
Brianne O'Gallagher-Roy <35981744+bogalla@users.noreply.github.com>
2021-07-16 16:26:40 -0400

    add title to metrics page (#18)
    Co-authored-by: Brianne <brianne@xguard.ca>

--- src/resources/js/components/backlog/backlogComponents/BacklogBar.vue
+++ src/resources/js/components/backlog/backlogComponents/BacklogBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-wrap p-4 pl-10">
-    <h3 class="text-3xl text-gray-800 font-bold py-1 pr-8">Backlog</h3>
+    <h3 class="text-3xl text-gray-800 font-bold py-1 pr-8">{{name}}</h3>
   </div>
 </template>
 
@@ -9,22 +9,11 @@ export default {
   inject: ["eventHub"],
   components: {},
   props: {
-    board: {
-      type: Array,
-      default: () => [],
+    name: {
+      type: String,
+      default: "",
     },
   },
   methods: {},
 };
 </script>
-
-<style scoped>
-.rotate-45 {
-  --transform-rotate: 45deg;
-  transform: rotate(45deg);
-}
-
-.group:hover .group-hover\:flex {
-  display: flex;
-}
-</style>
