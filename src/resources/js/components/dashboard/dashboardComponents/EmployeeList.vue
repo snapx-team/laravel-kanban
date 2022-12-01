493e2979ef5c6c827101a7f02a9bb2f3768f10a1
Siamak Samie <siamak.samie@gmail.com>
2022-12-01 01:31:21 -0500

    hotfix: dashboard lists pagination breaks
    when you select a new paginationIndex, in any of the lists in the dashboard, it would break when going to "next" page

--- src/resources/js/components/dashboard/dashboardComponents/EmployeeList.vue
+++ src/resources/js/components/dashboard/dashboardComponents/EmployeeList.vue
@@ -9,7 +9,7 @@
                         <div class="relative">
                             <select @change="paginationIndex = 0"
                                     class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                    v-model="paginationStep">
+                                    v-model.number="paginationStep">
                                 <option>5</option>
                                 <option>10</option>
                                 <option>20</option>
